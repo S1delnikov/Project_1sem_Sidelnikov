5f2ad65ff0c8d90425659fda574f89167c1b6532
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2021-12-10 20:59:08 +0300

    Update PZ_6_2.py

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,4 +1,5 @@
 # Дан целочисленный список размера N. Найти максимальное количество его одинаковых элементов.
+'''
 def pz():
     import random
     from collections import Counter
@@ -17,3 +18,28 @@ def pz():
     N = dict(Counter(N))
     print(N)
 pz()
+'''
+
+import random
+n = input('Введите размер списка: ')
+while type(n) != int:
+    try:
+        n = int(n)
+    except ValueError:
+        n = input('Это не число! Попробуйте снова: ')
+k = 1
+N = []
+while k <= n:
+        a = random.randint(0, 100)
+        N.append(a)
+        k+=1
+print(N)
+t = 0
+g = 0
+s = None
+for i in N:
+    t = N.count(i)
+    if t > g:
+        g = t
+        s = i
+print(s, 'встречается наибольшее количество раз, а именно', t, 'раз')
