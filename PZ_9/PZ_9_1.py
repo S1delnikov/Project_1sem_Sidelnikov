7869005b582e1d33a96f00db94b46d5daad3fb98
S1delnikov <misha.s1d@mail.ru>
2021-12-18 12:08:22 +0300

    PZ_9

---
+++ PZ_9/PZ_9_1.py
@@ -0,0 +1,11 @@
+# Известны марки машин, выпускаемые в данной стране и экспортируемых в N заданных стран. Определить какие марки машин
+# были досмтавлены во все указанные страны, какие в некоторые страны и какие не доставлены ни в одну страну.
+
+cars = {'BMW', 'Mercedec', 'Volvo', 'Audi'}
+russia = {'BMW', 'Mercedec'}
+france = {'Volvo', 'BMW'}
+poland = {'Mercedec', 'Volvo', 'BMW'}
+spain = {'Mercedec', 'Volvo', 'BMW'}
+print('Во все страны были доставлены: ', russia & france & poland & spain)
+print('Ни в одну страну не были доставлены: ', cars - russia - france - poland - spain)
+print('В некоторые страны были доставлены: ', russia | france | poland | spain)
