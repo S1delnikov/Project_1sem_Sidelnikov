1687b3ce6e1e7575d901b9cda5aa2b51753467ea
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2022-02-20 13:14:31 +0300

    Update PZ_13_2.py

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,1 +1,13 @@
+# Из заданной строки отобразить только символы нижнего регистра. Использовать библиотеку string.
 
+import string
+
+str = 'In PyCharm, you can specify third-patry ' \
+      'standalone applications and run them as ' \
+      'External Tools'
+symb = [' ', '-', '.', ':', ';','"']
+lst = [n for n in str if n in string.ascii_lowercase
+       or n in symb]
+print(str)
+print('_' * 100)
+print(''.join(lst))
